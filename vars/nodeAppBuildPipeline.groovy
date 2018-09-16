387dfcd928b7740e1c42145830a9f1b48fad9bf0
Marvin Andia <mandia1204@gmail.com>
2018-09-15 19:40:51 -0500

    first commit

---
+++ vars/nodeAppBuildPipeline.groovy
@@ -0,0 +1,30 @@
+def call(Map pipelineParams) {
+    pipeline {
+        agent any
+        tools {nodejs "node"}
+        stages {
+            stage('Install dependencies') {
+                steps {
+                    sh 'npm install'
+                }
+            }
+            stage('Build') {
+                steps {
+                    sh 'npm run build'
+                }
+            }
+            stage('Test') {
+                steps {
+                    ansiColor('xterm') {
+                        sh 'npm run test'
+                    }
+                }
+            }
+        }
+        post {
+            always {
+                archiveArtifacts 'dist/**/*'
+            }
+        }
+    }
+}
