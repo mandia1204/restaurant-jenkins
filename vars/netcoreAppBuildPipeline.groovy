b427d6a40f15e5fd4c22c90c2b857c35eb919c54
Marvin Andia <mandia1204@gmail.com>
2018-09-19 20:24:18 -0500

    netcore build pipeline

---
+++ vars/netcoreAppBuildPipeline.groovy
@@ -0,0 +1,35 @@
+def call(Map pipelineParams) {
+    pipeline {
+        agent any
+        stages {
+            stage('Restore packages') {
+                steps {
+                    sh "dotnet restore ${pipelineParams.projectSrcPath}"
+                }
+            }
+            stage('Build') {
+                steps {
+                    sh "dotnet restore ${pipelineParams.projectSrcPath}"
+                }
+            }
+            stage('Test') {
+                steps {
+                    ansiColor('xterm') {
+                        sh "dotnet test ${pipelineParams.projectSrcPath}"
+                    }
+                }
+            }
+            stage('Build release') {
+                steps {
+                    sh "dotnet publish ${pipelineParams.projectSrcPath} -c Release -o ../dist"
+                }
+            }
+        }
+        post {
+            always {
+                archiveArtifacts 'dist/**/*'
+            }
+        }
+    }
+}
+
