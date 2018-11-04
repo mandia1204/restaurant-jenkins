7ae28e6e3ed76598922c75d4121ecea283e692af
Marvin Andia <mandia1204@gmail.com>
2018-11-03 21:55:55 -0500

    update node deploy file

--- vars/nodeAppDeployPipeline.groovy
+++ vars/nodeAppDeployPipeline.groovy
@@ -2,29 +2,18 @@ import restaurant.util.*
 
 def call(Map params) {
     pipeline {
-        agent any
+        agent { label 'slave01' }
         stages {
             stage('Copy last successful artifact to ws') {
                 steps {
-                    script {
-                        sshUtil = new SSHUtil()
-                    }
                     copyArtifacts fingerprintArtifacts: true, projectName: "${params.projectName}", selector: lastSuccessful()
-                }
-            }
-            stage('Publish artifact to server') {
-                steps {
-                    script {
-                        sshUtil.publish configName: 'kube-server', removePrefix: 'dist', sourceFiles:'dist/**', dir: "${params.appName}/dist"
-                    }
                 }
             }
             stage('Build and publish image') {
                 steps {
                     script {
                         imageTag = TagGenerator.generateImageTag("${env.BUILD_NUMBER}")
-                        def command = "/restaurant/deploy/./build-image.sh -t ${imageTag} -a ${params.appName}"
-                        sshUtil.publish configName: 'kube-server', command: command
+                        sh "~/restaurant/deploy/./build-image.sh -t ${imageTag} -a ${params.appName}"
                     }
                 }
             }
@@ -32,6 +21,7 @@ def call(Map params) {
                 steps {
                     script {
                         def command = "/restaurant/deploy/./update-app.sh -t ${imageTag} -a ${params.appName}"
+                        def sshUtil = new SSHUtil()
                         sshUtil.publish configName: 'kube-server', command: command
                     }
                 }
