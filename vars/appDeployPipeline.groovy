58223ed5e123d50ed3a0396ae826ccab49a99d18
Marvin Andia <mandia1204@gmail.com>
2018-11-05 18:34:21 -0500

    rename deploy pipeline

--- vars/appDeployPipeline.groovy
+++ vars/appDeployPipeline.groovy
@@ -2,29 +2,23 @@ import restaurant.util.*
 
 def call(Map params) {
     pipeline {
-        agent any
+        agent { label 'slave01' }
         stages {
-            stage('Copy last successful artifact to ws') {
+            stage('Copy artifact to ws') {
                 steps {
-                    script {
-                        sshUtil = new SSHUtil()
-                    }
                     copyArtifacts fingerprintArtifacts: true, projectName: "${params.projectName}", selector: lastSuccessful()
                 }
             }
-            stage('Publish artifact to server') {
+            stage('Backup prev artifact and copy new from ws') {
                 steps {
-                    script {
-                        sshUtil.publish configName: 'kube-server', removePrefix: 'dist', sourceFiles:'dist/**', dir: "${params.appName}/dist"
-                    }
+                    sh "${env.HOME}/restaurant/deploy/./copy-artifact.sh -a ${params.appName} -w ${env.WORKSPACE}"
                 }
             }
             stage('Build and publish image') {
                 steps {
                     script {
                         imageTag = TagGenerator.generateImageTag("${env.BUILD_NUMBER}")
-                        def command = "/restaurant/deploy/./build-image.sh -t ${imageTag} -a ${params.appName}"
-                        sshUtil.publish configName: 'kube-server', command: command
+                        sh "${env.HOME}/restaurant/deploy/./build-image.sh -t ${imageTag} -a ${params.appName}"
                     }
                 }
             }
@@ -32,6 +26,7 @@ def call(Map params) {
                 steps {
                     script {
                         def command = "/restaurant/deploy/./update-app.sh -t ${imageTag} -a ${params.appName}"
+                        def sshUtil = new SSHUtil()
                         sshUtil.publish configName: 'kube-server', command: command
                     }
                 }
