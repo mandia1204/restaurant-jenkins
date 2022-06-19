b197cddf3c6acbbed7f80d9a1c506dd903575990
Marvin Andia <mandia1204@gmail.com>
2022-06-18 23:59:11 -0500

    tt

--- vars/nodeAppK8sDockerfileBuildPipeline.groovy
+++ vars/nodeAppK8sDockerfileBuildPipeline.groovy
@@ -15,14 +15,19 @@ def call(Map params) {
                     sh 'ls -l'
                     ansiColor('xterm') {
                         script {
-                            def imageTag = TagGenerator.generateImageTag("${env.BUILD_NUMBER}")
-                            def customImage = docker.build("${params.repoName}:${imageTag}", ".") // add -f ${dockerfile} if we need a differnet docker file name
-                            customImage.inside('-v /home/jenkins/agent/workspace/restaurant-security-build_master/report:/output -u root') {
-                                sh 'cp -R /var/www/report/* /output' // can see that test.html is generated
-                                sh 'ls -l /output'
-                            }
+                            imageTag = TagGenerator.generateImageTag("${env.BUILD_NUMBER}")
+                            docker.build("${params.repoName}:${imageTag}", ".") // add -f ${dockerfile} if we need a differnet docker file name
+                            // customImage.inside('-v /home/jenkins/agent/workspace/restaurant-security-build_master/report:/output -u root') {
+                            //     sh 'cp -R /var/www/report/* /output'
+                            //     sh 'ls -l /output'
+                            // }
                         }
                     }
+                    sh """
+                    docker run --name tocopy -d -u root ${params.repoName}:${imageTag}
+                    docker cp tocopy:/var/www/report/* ./report
+                    docker rm -f tocopy
+                    """
                     sh 'ls -l'
                     sh 'ls -l $WORKSPACE/report'
                 }
